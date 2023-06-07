1e9207cdba86c64089217668515e4c9bd0b73e92
Marco <mar.bustosb@duocuc.cl>
2023-06-07 14:52:25 -0400

    Add changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Inicio', url: 'inicio', icon: 'home' },
+    { title: 'Login', url: 'login', icon: 'log-in' },
+    { title: 'Mi Cuenta', url: 'cuenta', icon: 'person' },
+    { title: 'Mis Pedidos', url: 'compras', icon: 'gift' },
+    { title: 'Carrito', url: 'carrito', icon: 'cart' },
+    { title: 'Favoritos', url: 'favoritos', icon: 'heart' },
+  ];
+  public labels = ['Promociones', 'Descuentos'];
+  constructor() {}
+}
